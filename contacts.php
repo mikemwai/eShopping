ec65123a5b21aec248b2faa63c2d034fab5ba69f
MikeMwai <mwai.michael@strathmore.edu>
2022-07-27 09:34:58 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   3D.css 	new file:   Test8.html 	new file:   contacts.php

---
+++ contacts.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Question2(a)July2019</title>
+</head>
+<body>
+<a href="contacts_file/links.html">Links Page</a>
+<!--QUESTION 2--->
+<form action="submit.php"method="POST">
+<fieldset>
+<h1>Sign In Form </h1>
+<label for="username">Username:<span style="color:red">*</span></label>
+<input type="text"id="username"name="username"placeholder="Enter Username"><br></br>
+<label for="password">Password:<span style="color:red">*</span></label>
+<input type="password"id="password"name="password"placeholder="Enter password"><br></br>
+
+<input type="submit"value="Sign In">
+<input type="reset"value="Clear">
+</fieldset>
+</form>
+
+
+
+
+
+
+
+
+</body>
+</html>
+
